bd851a87eb013b494c8e506043a369f71e827c5c
Ingo Fischer <github@fischer-ka.de>
2018-06-16 23:06:08 +0200

    updae

--- main.js
+++ main.js
@@ -4,8 +4,12 @@
 /*jslint esversion: 6 */
 'use strict';
 
+var nodeFS = require('fs');
+var child_process = require('child_process');
 // you have to require the utils module and call adapter function
 var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var path = require('path');
+var dataDir = path.normalize(utils.controllerDir + path.sep + require(utils.controllerDir + path.sep + 'lib' + path.sep + 'tools').getDefaultDataDir());
 
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
@@ -13,6 +17,7 @@ var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
 var adapter = new utils.Adapter('ham');
 
 var homebridgeHandler;
+var attempts = {};
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
@@ -46,54 +51,72 @@ adapter.on('stateChange', function (id, state) {
     adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
     id = id.substr(adapter.namespace.length+1);
-    adapter.log.info('lookup id: ' + id);
+    adapter.log.debug('lookup id: ' + id);
     // you can use the ack flag to detect if it is status (true) or command (false)
     if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
+        adapter.log.debug('ack is not set!');
         homebridgeHandler.setValueForCharId(id, state.val);
     }
 });
 
-function updateDev(dev_id, dev_name, dev_type) {
+function updateDev(dev_id, dev_name, dev_type, dev_uuid) {
     adapter.log.info('updateDev ' + dev_id + ': name = ' + dev_name + ' /type= ' + dev_type);
     // create dev
     adapter.getObject(dev_id, function(err, obj) {
         if (!err && obj) {
             adapter.extendObject(dev_id, {
                 type: 'device',
-                common: {type: dev_type}
+                common: {name: dev_name},
+                native: {
+                    UUID: dev_uuid,
+                    displayName: dev_name,
+                    category: dev_type
+                }
             });
         }
         else {
             adapter.setObject(dev_id, {
                 type: 'device',
-                common: {name: dev_name, type: dev_type}
+                common: {name: dev_name},
+                native: {
+                    UUID: dev_uuid,
+                    displayName: dev_name,
+                    category: dev_type
+                }
             }, {});
         }
     });
 }
 
-function updateChannel(dev_id, ch_id, name, type) {
+function updateChannel(dev_id, ch_id, name, ch_uuid) {
     var id = dev_id + '.' + ch_id;
     // create channel for dev
-    adapter.log.info('updateChannel ' + id + ': name = ' + name + ' /type= ' + type);
+    adapter.log.info('updateChannel ' + id + ': name = ' + name);
     adapter.getObject(id, function(err, obj) {
         if (!err && obj) {
             adapter.extendObject(id, {
                 type: 'channel',
-                common: {type: type}
+                common: {name: name},
+                native: {
+                    UUID: ch_uuid,
+                    displayName: name
+                }
             });
         }
         else {
             adapter.setObject(id, {
                 type: 'channel',
-                common: {name: name, type: type}
+                common: {name: name},
+                native: {
+                    UUID: ch_uuid,
+                    displayName: name
+                }
             }, {});
         }
     });
 }
 
-function updateState(dev_id, ch_id, st_id, name, value, common) {
+function updateState(dev_id, ch_id, st_id, name, value, common, st_uuid, callback) {
     var id = dev_id + '.' + ch_id + '.'+ st_id;
     if (!common) common = {};
     if (common.name === undefined) common.name = name;
@@ -108,18 +131,24 @@ function updateState(dev_id, ch_id, st_id, name, value, common) {
         if (!err && obj) {
             adapter.extendObject(id, {
                 type: 'state',
-                common: common
-            });
+                common: common,
+                native: {
+                    UUID: st_uuid,
+                    displayName: name
+                }
+            }, callback);
         }
         else {
             adapter.setObject(id, {
                 type: 'state',
-                common: common
-            }, {});
+                common: common,
+                native: {
+                    UUID: st_uuid,
+                    displayName: name
+                }
+            }, callback);
         }
     });
-
-    adapter.setState(id, value, true);
 }
 
 
@@ -134,10 +163,28 @@ adapter.on('ready', function () {
     main();
 });
 
+function loadExistingAccessories(callback) {
+    adapter.getDevices(function(err, res) {
+        if (err) {
+            adapter.log.error('Can not get all existing devices: ' + err);
+            return;
+        }
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].native && res[i].native.UUID) {
+                adapter.log.debug('Remember existing Accessory ' + res[i].native.displayName + ' with UUID ' + res[i].native.UUID);
+                homebridgeHandler.registerExistingAccessory(res[i].native.UUID, res[i].native.displayName);
+            }
+        }
+
+        if (callback) callback();
+    });
+}
+
+
 
 function main() {
     if (adapter.config.useGlobalHomebridge) {
-        homebridgeHandler = require(__dirname + '/lib/globalhomebridge/handler');
+        homebridgeHandler = require(__dirname + '/lib/global-handler');
         homebridgeHandler.init({
             logger: adapter.log,
             homebridgeBasePath: adapter.config.globalHomebridgeBasePath,
@@ -149,12 +196,91 @@ function main() {
         });
     }
     else {
-        adapter.log.error('Non Global Mode not supported till now!!');
-        process.exit();
+        homebridgeHandler = require(__dirname + '/lib/wrapper-handler');
+        homebridgeHandler.init({
+            logger: adapter.log,
+            homebridgeConfigPath: dataDir + adapter.namespace.replace('.','_'), // /Users/ingof/.homebridge/
+            updateDev: updateDev,
+            updateChannel: updateChannel,
+            updateState: updateState,
+            setState: setState,
+            wrapperConfig: adapter.config.wrapperConfig
+        });
     }
-
-    homebridgeHandler.start();
 
     // in this template all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
+
+    installLibraries(function() {
+        loadExistingAccessories(function() {
+            homebridgeHandler.start();
+        });
+    });
+
+}
+
+
+
+
+function installNpm(npmLib, callback) {
+    const path = __dirname;
+    if (typeof npmLib === 'function') {
+        callback = npmLib;
+        npmLib = undefined;
+    }
+
+    const cmd = 'npm install ' + npmLib + ' --production --prefix "' + path + '"';
+    adapter.log.info(cmd + ' (System call)');
+    // Install node modules as system call
+
+    // System call used for update of js-controller itself,
+    // because during installation npm packet will be deleted too, but some files must be loaded even during the install process.
+    const child = child_process.exec(cmd);
+
+    child.stdout.on('data', function (buf) {
+        adapter.log.info(buf.toString('utf8'));
+    });
+    child.stderr.on('data', function (buf) {
+        adapter.log.error(buf.toString('utf8'));
+    });
+
+    child.on('exit', function (code /* , signal */) {
+        if (code) {
+            adapter.log.error('Cannot install ' + npmLib + ': ' + code);
+        }
+        // command succeeded
+        if (typeof callback === 'function') callback(npmLib);
+    });
+}
+
+function installLibraries(callback) {
+    let allInstalled = true;
+    if (adapter.config && adapter.config.libraries && !adapter.config.useGlobalHomebridge) {
+        const libraries = adapter.config.libraries.split(/[,;\s]+/);
+
+        for (let lib = 0; lib < libraries.length; lib++) {
+            if (libraries[lib] && libraries[lib].trim()) {
+                libraries[lib] = libraries[lib].trim();
+                if (!nodeFS.existsSync(__dirname + '/node_modules/' + libraries[lib] + '/package.json')) {
+
+                    if (!attempts[libraries[lib]]) {
+                        attempts[libraries[lib]] = 1;
+                    } else {
+                        attempts[libraries[lib]]++;
+                    }
+                    if (attempts[libraries[lib]] > 3) {
+                        adapter.log.error('Cannot install npm packet: ' + libraries[lib]);
+                        continue;
+                    }
+
+                    installNpm(libraries[lib], function () {
+                        installLibraries(callback);
+                    });
+                    allInstalled = false;
+                    break;
+                }
+            }
+        }
+    }
+    if (allInstalled) callback();
 }
---
+++ test/testAdapterWrapper.js
@@ -0,0 +1,274 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+var expect = require('chai').expect;
+var setup  = require(__dirname + '/lib/setup');
+
+var objects = null;
+var states  = null;
+var onStateChanged = null;
+var onObjectChanged = null;
+var sendToID = 1;
+
+var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.')+1);
+
+function checkConnectionOfAdapter(cb, counter) {
+    counter = counter || 0;
+    console.log('Try check #' + counter);
+    if (counter > 30) {
+        if (cb) cb('Cannot check connection');
+        return;
+    }
+
+    states.getState('system.adapter.' + adapterShortName + '.0.alive', function (err, state) {
+        if (err) console.error(err);
+        if (state && state.val) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkConnectionOfAdapter(cb, counter + 1);
+            }, 1000);
+        }
+    });
+}
+
+function checkValueOfState(id, value, cb, counter) {
+    counter = counter || 0;
+    if (counter > 20) {
+        if (cb) cb('Cannot check value Of State ' + id);
+        return;
+    }
+
+    states.getState(id, function (err, state) {
+        if (err) console.error(err);
+        if (value === null && !state) {
+            if (cb) cb();
+        } else
+        if (state && (value === undefined || state.val === value)) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkValueOfState(id, value, cb, counter + 1);
+            }, 500);
+        }
+    });
+}
+
+function sendTo(target, command, message, callback) {
+    onStateChanged = function (id, state) {
+        if (id === 'messagebox.system.adapter.test.0') {
+            callback(state.message);
+        }
+    };
+
+    states.pushMessage('system.adapter.' + target, {
+        command:    command,
+        message:    message,
+        from:       'system.adapter.test.0',
+        callback: {
+            message: message,
+            id:      sendToID++,
+            ack:     false,
+            time:    (new Date()).getTime()
+        }
+    });
+}
+
+describe('Test ' + adapterShortName + ' adapter', function() {
+    before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
+        this.timeout(600000); // because of first install from npm
+
+        setup.setupController(function () {
+            var config = setup.getAdapterConfig();
+            // enable adapter
+            config.common.enabled  = true;
+            config.common.loglevel = 'debug';
+
+            config.native.useGlobalHomebridge = false;
+            config.native.globalHomebridgeConfigPath = __dirname + "/homebridge/";
+            config.native.libraries = "homebridge-http-webhooks homebridge-sun-position";
+            config.native.wrapperConfig = {
+                "accessories": [
+            		{
+                        "accessory" : "SunPosition",
+                        "name" : "Sun",
+                        "location" : {
+                        	"lat" : 49.035924,
+                        	"long" : 8.345736
+                        }
+                    }
+                ],
+
+                "platforms": [
+            		{
+            			"platform": "HttpWebHooks",
+            			"webhook_port": "61828",
+            			"cache_directory": "./.node-persist/storage",
+            			"sensors": [
+            				{
+            				"id": "sensor1",
+            				"name": "Sensor name 1",
+            				"type": "contact"
+            				},
+            				{
+            				"id": "sensor2",
+            				"name": "Sensor name 2",
+            				"type": "motion"
+            				},
+            				{
+            				"id": "sensor3",
+            				"name": "Sensor name 3",
+            				"type": "occupancy"
+            				},
+            				{
+            				"id": "sensor4",
+            				"name": "Sensor name 4",
+            				"type": "smoke"
+            				},
+            				{
+            				"id": "sensor5",
+            				"name": "Sensor name 5",
+            				"type": "temperature"
+            				},
+            				{
+            				"id": "sensor6",
+            				"name": "Sensor name 6",
+            				"type": "humidity"
+            				},
+            				{
+            				"id": "sensor7",
+            				"name": "Sensor name 7",
+            				"type": "airquality"
+            				},
+            				{
+            				"id": "sensor8",
+            				"name": "Sensor name 8",
+            				"type": "airquality"
+            				}
+            			],
+            			"switches": [
+            				{
+            				"id": "switch1",
+            				"name": "Switch name 1",
+            				"on_url": "http://localhost:9080/switch1?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/switch1?off",
+            				"off_method": "GET"
+            				},
+            				{
+            				"id": "switch2",
+            				"name": "Switch name 2",
+            				"on_url": "http://localhost:9080/switch2?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/switch2?off",
+            				"off_method": "GET"
+            				},
+            				{
+            				"id": "switch3",
+            				"name": "Switch name 3",
+            				"on_url": "http://localhost:9080/switch3?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/switch3?off",
+            				"off_method": "GET"
+            				},
+            				{
+            				"id": "switch4",
+            				"name": "Switch name*3",
+            				"on_url": "http://localhost:9080/switch3-2?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/switch3-2?off",
+            				"off_method": "GET"
+            				}
+            			],
+            			"pushbuttons": [
+            				{
+            				"id": "pushbutton1",
+            				"name": "Push button name 1",
+            				"push_url": "http://localhost:9080/pushbutton1?push",
+            				"push_method": "GET"
+            				}
+            			],
+            			"lights": [
+            				{
+            				"id": "light1",
+            				"name": "Light name 1",
+            				"on_url": "http://localhost:9080/light1?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/light1?off",
+            				"off_method": "GET"
+            				}
+            			],
+            			"thermostats": [
+            				{
+            				"id": "thermostat1",
+            				"name": "Thermostat name 1",
+            				"set_target_temperature_url": "http://localhost:9080/thermostat1?targettemperature=%f",
+            				"set_target_heating_cooling_state_url": "http://localhost:9080/thermostat1??targetstate=%b"
+            				}
+            			],
+            			"outlets": [
+            				{
+            				"id": "outlet1",
+            				"name": "Outlet name 1",
+            				"on_url": "http://localhost:9080/outlet1?on",
+            				"on_method": "GET",
+            				"off_url": "http://localhost:9080/outlet1?off",
+            				"off_method": "GET"
+            				}
+            			]
+            		}
+                ]
+            };
+
+            setup.setAdapterConfig(config.common, config.native);
+
+            setup.startController(true, function(id, obj) {}, function (id, state) {
+                    if (onStateChanged) onStateChanged(id, state);
+                },
+                function (_objects, _states) {
+                    objects = _objects;
+                    states  = _states;
+                    _done();
+                });
+        });
+    });
+
+/*
+    ENABLE THIS WHEN ADAPTER RUNS IN DEAMON MODE TO CHECK THAT IT HAS STARTED SUCCESSFULLY
+*/
+    it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
+        this.timeout(60000);
+        checkConnectionOfAdapter(function (res) {
+            if (res) console.log(res);
+            expect(res).not.to.be.equal('Cannot check connection');
+            objects.setObject('system.adapter.test.0', {
+                    common: {
+
+                    },
+                    type: 'instance'
+                },
+                function () {
+                    states.subscribeMessage('system.adapter.test.0');
+                    done();
+                });
+        });
+    });
+/**/
+
+/*
+    PUT YOUR OWN TESTS HERE USING
+    it('Testname', function ( done) {
+        ...
+    });
+
+    You can also use "sendTo" method to send messages to the started adapter
+*/
+
+    after('Test ' + adapterShortName + ' adapter: Stop js-controller', function (done) {
+        this.timeout(10000);
+
+        setup.stopController(function (normalTerminated) {
+            console.log('Adapter normal terminated: ' + normalTerminated);
+            done();
+        });
+    });
+});
