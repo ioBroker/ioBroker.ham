b4944443d4a534b37e446e82f71eb1fc66ad2d6d
Ingo Fischer <github@fischer-ka.de>
2018-12-17 09:09:21 +0100

    try fix travis windows build, install OpenSSL manually

---
+++ test/OpenSSL102-choco/tools/chocolateyInstall.ps1
@@ -0,0 +1,37 @@
+$package = 'OpenSSL102-choco'
+
+try {
+
+  #default is to plop in c:\ -- yuck!
+  $installDir = Join-Path $Env:ProgramFiles 'OpenSSL'
+
+  $params = @{
+    packageName = $package;
+    fileType = 'exe';
+    #InnoSetup - http://unattended.sourceforge.net/InnoSetup_Switches_ExitCodes.html
+    silentArgs = '/silent', '/verysilent', '/sp-', '/suppressmsgboxes',
+      "/DIR=`"$installDir`"";
+    url = 'https://slproweb.com/download/Win32OpenSSL-1_0_2n.exe'
+    url64bit = 'https://slproweb.com/download/Win64OpenSSL-1_0_2n.exe'
+  }
+
+  Install-ChocolateyPackage @params
+
+  if (!$Env:OPENSSL_CONF)
+  {
+    $configPath = Join-Path $installDir 'bin\openssl.cfg'
+
+    if (Test-Path $configPath)
+    {
+      [Environment]::SetEnvironmentVariable(
+        'OPENSSL_CONF', $configPath, 'User')
+
+      Write-Host "Configured OPENSSL_CONF variable as $configPath"
+    }
+  }
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
