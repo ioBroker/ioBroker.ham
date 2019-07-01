aa79424635b3b6683e0f18792ee1f7d16d82bb63
Ingo Fischer <github@fischer-ka.de>
2019-07-01 23:41:01 +0200

    try to get testing running

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -172,14 +172,14 @@ function checkIsControllerInstalled(cb, counter) {
             return;
         }
     } catch (err) {
-
+        console.log(err);
     }
 
     if (counter > 20) {
         console.log('checkIsControllerInstalled: Cannot install!');
         if (cb) cb('Cannot install');
     } else {
-        console.log('checkIsControllerInstalled: wait...');
+        console.log('checkIsControllerInstalled: wait... ' + counter);
         setTimeout(function() {
             checkIsControllerInstalled(cb, counter + 1);
         }, 1000);
