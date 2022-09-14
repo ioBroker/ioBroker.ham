731f708f154cb4e8a3b32477f61896386a2908b3
Ingo Fischer <github@fischer-ka.de>
2022-09-14 13:17:47 +0200

    * re-adjust logging

--- main.js
+++ main.js
@@ -177,18 +177,6 @@ function createHam(options) {
         });
     }
 
-    function hook_stream(_stream, fn) {
-        // Reference default write method
-        const old_write = _stream.write;
-        // _stream now write with our shiny function
-        _stream.write = fn;
-
-        return function() {
-            // reset to the default write method
-            _stream.write = old_write;
-        };
-    }
-
     //Catch Homebridge Console Logging
     if (process.argv.indexOf('--logs') === -1 && process.argv.indexOf('-l') === -1) {
         console.log = function (logs) {
